77b9f3ff5c48c93c61f285f28871b016581fd15c
chirchib <58251292+chirchib@users.noreply.github.com>
2020-02-10 22:56:51 +0300

    Create quadrangle.cpp

---
+++ quadrangle.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include "quadrangle.h"
+using namespace std;
+
+
+Quadrangle::Quadrangle() {
+	
+}
+
+void Quadrangle::setCoordinates() {
+	
+}
+
+void Quadrangle::getSides() {
+	
+}
+
+void Quadrangle::perimetr() {
+	
+}
+
+void Quadrangle::area() {
+	
+}
+
+bool Quadrangle::checkingRectangle() {
+	
+}
+
+void Quadrangle::findingSides() {
+	
+}
